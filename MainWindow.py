bb284d4c4f771b7fe2b78e8b5f1c0a0688a32071
Davidelcraack <73145779+Davidelcraack@users.noreply.github.com>
2023-11-18 21:29:14 -0600

    Add files via upload

--- MainWindow.py
+++ MainWindow.py
@@ -1,3 +1,5 @@
+import cv2
+from PIL import Image, ImageTk
 import tkinter as tk
 from tkinter import Menu, Toplevel
 
@@ -40,16 +42,19 @@ class MainWindow:
         frame_derecha = tk.Frame(frame, bg="#EFEAE9")
         frame_derecha.pack(side="left")
 
-        canvas = tk.Canvas(frame_derecha, width=750, height=750, bg="#EFEAE9")
-        canvas.pack()
-        canvas.create_rectangle(5, 5, 750, 750, outline="#202123", width=5)
+        self.canvas = tk.Canvas(frame_derecha, width=750, height=750, bg="#EFEAE9")
+        self.canvas.pack()
+        self.canvas.create_rectangle(5, 5, 750, 750, outline="#202123", width=5, tag="camara")
 
-        self._set_menubar(
-            [
-                {"label": "Add employee", "command": self.crear_usuario},
-                {"label": "Delete employee", "command": self.eliminar_usuario},
-            ]
-        )
+        self._set_menubar([
+            {"label": "Add employee", "command": self.crear_usuario},
+            {"label": "Delete employee", "command": self.eliminar_usuario},
+        ])
+
+        # Inicialización de la cámara y llamada a la función para actualizar el rectángulo
+        self.cap = cv2.VideoCapture(0)
+        self.photo = None  # Agregamos esta línea
+        self.update_camera()
 
     def _set_menubar(self, commands: list):
         menubar = Menu(self.window)
@@ -73,51 +78,19 @@ class MainWindow:
     def start_window(self):
         self.window.mainloop()
 
+    def update_camera(self):
+        ret, frame = self.cap.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(frame)
+            self.photo = ImageTk.PhotoImage(image=img)
+
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW, tag="camara")
+            self.window.after(10, self.update_camera)
+        else:
+            self.window.after(10, self.update_camera)
+
     def crear_usuario(self):
         crear_usuario = Toplevel(self.window, bg="#EFEAE9")
         crear_usuario.geometry("400x300" )
-        crear_usuario.title("Crear usuario")
-
-        # Etiqueta y entrada para el nombre
-        lbl_nombre = tk.Label(crear_usuario, text="Ingresa tu nombre:")
-        lbl_nombre.pack(pady=10)
-        entry_nombre = tk.Entry(crear_usuario, width=30)
-        entry_nombre.pack(pady=5)
-
-        # Etiqueta y entrada para el apeido paterno
-        lbl_nombre = tk.Label(crear_usuario, text="Ingresa tu apeido paterno:")
-        lbl_nombre.pack(pady=10)
-        entry_apeido1 = tk.Entry(crear_usuario, width=30)
-        entry_apeido1.pack(pady=5)
-
-        # Etiqueta y entrada para el apeido paterno
-        lbl_nombre = tk.Label(crear_usuario, text="Ingresa tu apeido materno:")
-        lbl_nombre.pack(pady=10)
-        entry_apeido2 = tk.Entry(crear_usuario, width=30)
-        entry_apeido2.pack(pady=5)
-
-
-        # Lambda se utiliza para pasar los valores de entry_nombre y entry_contrasena
-        btn_aceptar = tk.Button(crear_usuario, text="Aceptar", command=lambda: self.guardar_usuario(entry_nombre.get(), entry_contrasena.get()))
-        btn_aceptar.pack(pady=20)    
-
-    def eliminar_usuario(self):
-        # Obtener el nombre del usuario a eliminar
-        nombre_usuario = self.entry_nombre.get()
-
-        # Aquí puedes agregar la lógica para eliminar el usuario con el nombre proporcionado
-        print(f"Usuario eliminado: {nombre_usuario}")
-
-    def verificar(self):
-        # Agrega la lógica para verificar aquí
-        pass
-
-    def guardar_usuario(self, nombre, contrasena):
-        # Guardar la información del usuario en un archivo txt
-        with open('usuarios.json', 'a') as archivo:
-            archivo.write(f"{nombre},{contrasena}\n")
-        print("Usuario guardado exitosamente.")
-
-# Crear una instancia de la clase MainWindow y mostrar la ventana
-app = MainWindow()
-app.start_window()
+        crear_usuario.title("Crear")
